2c4b775bf25952e219d24baf3cdc44c648902981
Dingjiang Zhou <zhoudingjiang@gmail.com>
2021-06-12 00:39:39 -0400

    add service server and client

--- src/listener-node.cpp
+++ src/listener-node.cpp
@@ -1,8 +1,21 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "std_srvs/srv/empty.hpp"
+#include <iostream>
+
+using namespace std::chrono_literals;
+using namespace rclcpp;
+
+Client<std_srvs::srv::Empty>::SharedPtr client;
 
 void chatterCallback(const std_msgs::msg::String::SharedPtr msg) {
     std::cout << "I heard: [" << msg->data << "]" << std::endl;
+}
+
+void timerCallback() {
+    auto request = std::make_shared<std_srvs::srv::Empty::Request>();
+    auto result  = client->async_send_request(request);
+    ( void )result;
 }
 
 int main(int argc, char* argv[]) {
@@ -11,7 +24,9 @@ int main(int argc, char* argv[]) {
     auto node = rclcpp::Node::make_shared("listener");
 
     auto chatter_sub = node->create_subscription<std_msgs::msg::String>(
-        "chatter", 10, &chatterCallback);
+        "/chatter", 10, &chatterCallback);
+    auto timer = node->create_wall_timer(5s, &timerCallback);
+    client     = node->create_client<std_srvs::srv::Empty>("toggle");
 
     rclcpp::spin(node);
 
--- src/talker-node.cpp
+++ src/talker-node.cpp
@@ -1,18 +1,37 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "std_srvs/srv/empty.hpp"
+
+bool talk_style;
+
+void serviceCallback(
+    const std::shared_ptr<std_srvs::srv::Empty::Request> request,
+    std::shared_ptr<std_srvs::srv::Empty::Response> response) {
+    ( void )request;
+    ( void )response;
+    talk_style = !talk_style;
+}
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("talker");
+    talk_style = true;
+    auto node  = rclcpp::Node::make_shared("talker");
     auto chatter_pub =
-        node->create_publisher<std_msgs::msg::String>("chatter", 10);
+        node->create_publisher<std_msgs::msg::String>("/chatter", 10);
+    auto service =
+        node->create_service<std_srvs::srv::Empty>("/toggle", &serviceCallback);
 
     rclcpp::WallRate loop_rate(2);
 
     auto message = std_msgs::msg::String();
     auto count   = 0;
     while (rclcpp::ok()) {
-        message.data = "Hello, ROS2 and AGV! " + std::to_string(count++);
+        if (talk_style) {
+            message.data = "Hello, ROS2 and AGV! " + std::to_string(count++);
+        }
+        else {
+            message.data = std::to_string(count++) + ": It's a good day today!";
+        }
         RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Publishing: '%s'",
                     message.data.c_str());
 
